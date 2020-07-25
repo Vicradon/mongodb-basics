fa51bf591b51bf21f058546c7fccd992c4b1055c
Osinachi Chukwujama <vicraph7@gmail.com>
2020-07-25 03:04:06 +0000

    COMPLETE TASK 1

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http');
-console.log("Server is listen on port 2000", 'localhost:2000')
+const PORT = 2000
+console.log(`Server is listen on port ${PORT}`, `http://localhost:${PORT}`)
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write("Some text");
-  res.end()
-}).listen(2000);
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end("Hello World, Welcome to WeJapa Internships")
+  }
+  if (req.method === 'POST' && req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+
+    req.on('data', chunk => {
+      const body = chunk.toString();
+
+      const { name } = JSON.parse(body)
+      res.end(`Hello ${name}, Welcome to WeJapa Internships`)
+    });
+  }
+}).listen(PORT);
